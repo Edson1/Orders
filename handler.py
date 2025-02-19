1acf41ce65aa4b22d3ea391c4098f172ad4927c0
Edson1 <Edsonfs12@hotmail.com>
2025-02-19 09:49:31 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -12,26 +12,44 @@ dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
 
 SQS_QUEUES = {
     "recibida": os.environ.get('ORDERSQUEUERECEIVED_QUEUE_URL'), #"SQS_QUEUE_RECEIVED",    
-    "enproceso": os.environ.get('ORDERSQUEUEINPROCESS_QUEUE_URL'), #"SQS_QUEUE_IN_PROCESS", ###URL
-    "completada": os.environ.get('ORDERSQUEUECOMPLETED_QUEUE_URL'), #"SQS_QUEUE_COMPLETED", #### os.environ.get('SQS_QUEUE_IN_PROCESS')
+    "enproceso": os.environ.get('ORDERSQUEUEINPROCESS_QUEUE_URL'), #"SQS_QUEUE_IN_PROCESS", 
+    "completada": os.environ.get('ORDERSQUEUECOMPLETED_QUEUE_URL'), #"SQS_QUEUE_COMPLETED", 
     "cancelada": os.environ.get('ORDERSQUEUECANCELLED_QUEUE_URL') #"SQS_QUEUE_CANCELLED"
 }
 
 sqs = boto3.client('sqs', region_name=AWS_REGION)
 
-def OrderLambda(event, context):
-
-    # parse json data
-    body = json.loads(event['body'])
+def validate_input(body):
+    required_fields = ["fecha_entrega", "estado", "descripcion"]
     
-    # verify post variables
-    validateBody = body["fecha_entrega"]
+    # Verificar si el cuerpo es un diccionario válido
+    if not isinstance(body, dict):
+        return "Invalid request body JSON format"
+    
+    # Verificar que todos los campos requeridos existen 
+    for field in required_fields:
+        if field not in body  or  not isinstance(body[field], str):
+            return f"Missing or invalid '{field}' in request body."
 
-    if len(validateBody) < 1:
+    # Validar fecha_entrega (ejemplo de formato YYYY-MM-DD)
+    try:
+        datetime.datetime.strptime(body['fecha_entrega'], "%d-%m-%Y")
+    except ValueError:
+        return "Invalid 'fecha_entrega' format. Expected DD-MM-YYYY"
+    
+    return "valid"
+
+def OrderLambda(event, context):     
+
+    body = json.loads(event['body'])
+
+    # Validar input data
+    message = validate_input(body)
+    if message != "valid":
         return {
             'statusCode': 400,
-            'body': json.dumps({'message': 'Missing input data, please verify the request body'})
-        }
+            'body': json.dumps({'message': 'Input Validation error', 'error': message})
+        }   
     
     # requerir motivo de cancelacion de orden
     if body['estado'].lower() == "cancelada":
@@ -41,12 +59,22 @@ def OrderLambda(event, context):
                 'body': json.dumps({'message': 'Agregar el motivo de cancelacion de la Orden en la Descripcion, por favor'})
             }
 
-    orderId = uuid.uuid4()
-    print('Order ID: ' + str(orderId))
+    # Determinar si se genera un nuevo orderId o se usa el proporcionado
+    if body["estado"].lower() == "recibida":
+        orderId = str(uuid.uuid4())  # Generar un nuevo ID solo para "recibida"
+    else:
+        if "id" not in body or not body["id"]:  # Si no proporciona id, error
+            return {
+                    'statusCode': 400,
+                    'body': json.dumps({'message': 'El campo id es obligatorio para actualizar una orden existente'})
+            }
+        orderId = body["id"]  # Usar el ID proporcionado por el frontend
+
+    print('Order ID: ' + orderId)
 
     try:
         order = {
-            'id': str(orderId),
+            'id': orderId,
             'fecha_registro': datetime.datetime.now().isoformat(),
             'timestamp':  str(datetime.datetime.now().timestamp()),
             'descripcion': body['descripcion'],
@@ -54,9 +82,29 @@ def OrderLambda(event, context):
             'estado': body['estado']
         }
 
-        # Guardar en DynamoDB
+        # Operación en DynamoDB
         table = dynamodb.Table(DYNAMODB_TABLE)
-        table.put_item(Item=order)
+
+        if body["estado"].lower() == "recibida":
+            # Insertar nueva orden
+            table.put_item(Item=order)
+        else:
+            table.update_item(
+                Key={'id': orderId},
+                UpdateExpression="SET fecha_registro = :fecha_registro, "
+                                "timestamp = :timestamp, "
+                                "descripcion = :descripcion, "
+                                "fecha_entrega = :fecha_entrega, "
+                                "estado = :estado",
+                ExpressionAttributeValues={
+                    ':fecha_registro': order['fecha_registro'],
+                    ':timestamp': order['timestamp'],
+                    ':descripcion': order['descripcion'],
+                    ':fecha_entrega': order['fecha_entrega'],
+                    ':estado': order['estado']
+                },
+                ConditionExpression="attribute_exists(id)"  # Asegura que la orden ya existe antes de actualizar
+            )
                 
         # Enviar order a SQS según estado
         queue_url = SQS_QUEUES.get(order['estado'].lower())
@@ -78,5 +126,3 @@ def OrderLambda(event, context):
             'body': json.dumps({'message': 'Error al procesar la orden', 'error': str(e)})
         }
     
-
-
